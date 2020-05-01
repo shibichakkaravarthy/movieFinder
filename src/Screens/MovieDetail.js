d4a507d3a2b150a313c4c104bb9ae1cd16303a7d
shibichakkaravarthy <shibi2591999@gmail.com>
2020-05-01 20:28:58 +0530

    Added Search in Screen 2

--- src/Screens/MovieDetail.js
+++ src/Screens/MovieDetail.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import { SafeAreaView, ScrollView, View, TouchableOpacity, Dimensions, Image, Linking } from 'react-native'
 import { shallowEqual, useSelector, useDispatch } from 'react-redux'
-import { Icon, Input, Text, Button, Spinner } from 'native-base'
+import { Icon, Input, Text, Button, Spinner, Item, Picker } from 'native-base'
 import LinearGradient from 'react-native-linear-gradient';
-import { updateMovieData } from '../Components/Actions'
+import { fetchMovies, getMovieDetail, onSearch, setSuggestionsToMovies, resetSuggestions, updateMovieData } from '../Components/Actions'
 
 import { ScreenHeader } from '../Components'
 
@@ -12,7 +12,7 @@ console.log('Dim', width, height)
 
 export const MovieDetail = ({ navigation }) => {
 
-	const { selectedMovie } = useSelector(state => {
+	const { selectedMovie, movies, searchTerm, searchYear, searchSuggestions } = useSelector(state => {
 		// console.log('state', state)
 		return state.movies
 	})
@@ -38,9 +38,62 @@ export const MovieDetail = ({ navigation }) => {
 		Linking.openURL(`https://imdb.com/title/${id}/`)
 	}
 
+	const currentYear = parseInt(new Date().getFullYear()) - 1870 //Oldest Movie in IMDb is dated back to 1874
+
 	return (
 		<SafeAreaView style={[ Styles.backgroundBlack, Styles.flex1 ]} >
 			<ScreenHeader title={selectedMovie.Title} goBack={() => { onGoBack() }} openImdb={() => onOpenImdb(selectedMovie.imdbID)} />
+			
+			<View  style={[ Styles.margin20, Styles.flexRow, Styles.alignCenter, Styles.justifySpaceAround ]} >
+				<Item style={[ Styles.flex3 ]} >
+					<Icon style={[ Styles.fontColorWhite ]} active name='ios-search' type='Ionicons' />
+					<Input value={searchTerm} placeholder='Search' onChangeText={text => dispatch(onSearch(text))} style={[ Styles.fontColorWhite ]} />
+				</Item>
+
+				<Item picker style={[ Styles.flex2, { marginLeft: 10 } ]} >
+					<Picker mode="dropdown" style={{ width: undefined, color: '#fff' }} itemStyle={[ Styles.backgroundBlack ]} selectedValue={searchYear} onValueChange={year => dispatch(updateMovieData('searchYear', year))} >
+						<Picker.Item label='All Years' value={false} />
+						{
+							Array.from(Array(currentYear).keys()).map((x, i) => {
+								return <Picker.Item key={x} label={(parseInt(new Date().getFullYear()) - x).toString()} value={(parseInt(new Date().getFullYear()) - x).toString()} />
+							})
+						}
+					</Picker>
+				</Item>
+
+				<TouchableOpacity onPress={() => dispatch(setSuggestionsToMovies())} >
+					<Icon style={[ Styles.fontColorWhite ]} active name='arrowright' type='AntDesign' />
+				</TouchableOpacity>
+			</View>
+
+			<View style={[{ position: 'relative' }, Styles.padding10]} >
+				{
+					(searchSuggestions && searchSuggestions.length)
+					?
+					<ScrollView keyboardShouldPersistTaps="always" style={[{ position: 'absolute', borderBottomLeftRadius: 20, borderBottomRightRadius: 20, zIndex: 99, left: 0, right: 0, maxHeight: height*0.5 }, Styles.backgroundBlack, Styles.borderColorBlue, Styles.border1, Styles.padding10 ]} >
+						{
+							searchSuggestions.map(movie => {
+								return (
+									<TouchableOpacity key={movie.Title} onPress={() => { dispatch(getMovieDetail(movie.imdbID)), dispatch(resetSuggestions()), navigation.navigate('Movie Detail') }} >
+										<View style={[ Styles.flexRow, Styles.alignCenter, Styles.padding10, Styles.margin5]} >
+											<View>
+												<Image style={{ width: 40, height: 40 }} source={{ uri: movie.Poster }} />
+											</View>
+											<View style={{ paddingLeft: 10 }} >
+												<Text style={[ Styles.fontColorWhite ]} >{movie.Title}</Text>
+												<Text style={[ Styles.fontColorWhite ]} >{movie.Year}</Text>
+											</View>
+										</View>
+									</TouchableOpacity>
+								)
+							})
+						}
+					</ScrollView>
+					:
+					null
+				}
+			</View>
+
 			<ScrollView style={[ Styles.flex1 ]} >
 				<View style={[ Styles.alignCenter, Styles.margin10 ]} >
 					<Image style={[{ width: width*0.8, height: (444*width*0.8)/300 }, Styles.borderRadius10]} source={{ uri: selectedMovie.Poster }} />
